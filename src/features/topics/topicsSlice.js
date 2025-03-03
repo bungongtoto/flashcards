64d149428caaba7984a18acce1faf35888700c1d
bungongtoto <bungongkingsley@gmail.com>
2025-03-03 11:51:30 +0000

    add addQuiz

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,19 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const topicsSlice = createSlice({
-    name: 'topics',
-    initialState: {
-        topics: {}
+  name: "topics",
+  initialState: {
+    topics: {},
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const newtopicObject = { ...action.payload, quizIds: [] };
+      state.topics[action.payload.id] = newtopicObject;
     },
-    reducers: {
-        addTopic: (state, action) => {
-            const newtopicObject = {...action.payload, quizIds: []}
-            state.topics[action.payload.id] = newtopicObject
-        }
-    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addQuiz, (state, action) => {
+        state.topics[action.payload.topicId].quizIds.push(action.payload.id);
+    });
+  },
 });
 
-export const {addTopic} = topicsSlice.actions;
+export const { addTopic, addQuiz } = topicsSlice.actions;
 
 export const selectAllTopics = (state) => state.topics.topics;
 
